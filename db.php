6a6d5e2cd86bbda8e73477a8d722f13d89b95c2c
조형근 <91794936+woomae@users.noreply.github.com>
2023-06-23 12:56:10 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 # 주소 아이디, 비밀번호, 데이터베이스
-$db = mysqli_connect('localhost','root','gudrms01','level1');
+$db = mysqli_connect('localhost','root','password','level1');
 if(!$db){
     echo "DB접속실패";
 }
